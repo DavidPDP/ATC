001424b7562db66e96c863609f1597cec6769ffa
mgltorsa <mgltorsa@gmail.com>
2020-06-26 01:52:47 -0500

    added evaluated expression as evaluator's entity

--- src/main/java/co/edu/icesi/metrocali/atc/entities/evaluator/EvaluatedExpression.java
+++ src/main/java/co/edu/icesi/metrocali/atc/entities/evaluator/EvaluatedExpression.java
@@ -1,4 +1,4 @@
-package co.edu.icesi.metrocali.atc.evaluator.dto;
+package co.edu.icesi.metrocali.atc.entities.evaluator;
 
 import org.springframework.expression.ExpressionException;
 import lombok.AllArgsConstructor;
@@ -8,28 +8,28 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class EvalEvaluatedExpressionDTO {
+public class EvaluatedExpression {
 
     private String expression;
     private String valueType;
     private Object value;
 
-    public EvalEvaluatedExpressionDTO(String expression, Object value) {
+    public EvaluatedExpression(String expression, Object value) {
         this.expression = expression;
         this.value = value;
         this.valueType = value.getClass().getName();
     }
 
-    public EvalEvaluatedExpressionDTO(String expression, ExpressionException exception) {
+    public EvaluatedExpression(String expression, ExpressionException exception) {
         this.expression = expression;
         this.value = exception.toDetailedString();
         this.valueType = exception.getClass().getName();
     }
 
-    public static EvalEvaluatedExpressionDTO getEvalEvaluatedExpressionResult(String expression,
+    public static EvaluatedExpression getEvalEvaluatedExpressionResult(String expression,
             ExpressionException e) {
 
-        return new EvalEvaluatedExpressionDTO(expression, e);
+        return new EvaluatedExpression(expression, e);
     }
 
 }
