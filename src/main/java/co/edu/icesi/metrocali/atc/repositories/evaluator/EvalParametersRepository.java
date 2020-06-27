cdab27bcba72973003b19f09fd0e8f6f66868598
mgltorsa <mgltorsa@gmail.com>
2020-06-27 14:37:54 -0500

    updated update method. Added parameter name as a segment path

--- src/main/java/co/edu/icesi/metrocali/atc/repositories/evaluator/EvalParametersRepository.java
+++ src/main/java/co/edu/icesi/metrocali/atc/repositories/evaluator/EvalParametersRepository.java
@@ -15,98 +15,103 @@ import co.edu.icesi.metrocali.atc.entities.evaluator.EvalParameter;
 @Repository
 public class EvalParametersRepository extends EvaluatorRepository {
 
-    private static final String PARAMETERS_URL = "/parameters";
-    private static final String ACTIVE_URL_PATH = "/active";
-    private static final String FILTERED_URL_PATH = "/filtered";
+        private static final String PARAMETERS_URL = "/parameters";
+        private static final String ACTIVE_URL_PATH = "/active";
+        private static final String FILTERED_URL_PATH = "/filtered";
 
-    private static final String ENABLE_FROM_PARAM = "enable_from";
-    private static final String ENABLE_UNTIL_PARAM = "enable_until";
-    private static final String ACTIVE_PARAM = "active";
+        private static final String ENABLE_FROM_PARAM = "enable_from";
+        private static final String ENABLE_UNTIL_PARAM = "enable_until";
+        private static final String ACTIVE_PARAM = "active";
 
-    public EvalParametersRepository(@Qualifier("blackboxApi") RestTemplate blackboxApi,
-            @Value("${blackbox.apis.evaluator}") String blackboxEvaluatorApiURL) {
-        super(blackboxApi, blackboxEvaluatorApiURL);
-    }
+        public EvalParametersRepository(@Qualifier("blackboxApi") RestTemplate blackboxApi,
+                        @Value("${blackbox.apis.evaluator}") String blackboxEvaluatorApiURL) {
+                super(blackboxApi, blackboxEvaluatorApiURL);
+        }
 
-    public EvalParameter retrieveActiveByName(String name) {
-        UriComponentsBuilder uriBuilder =
-                UriComponentsBuilder.fromHttpUrl(blackboxEvaluatorApiURL + PARAMETERS_URL);
-        uriBuilder.pathSegment(name);
-        uriBuilder.path(ACTIVE_URL_PATH);
+        public EvalParameter retrieveActiveByName(String name) {
+                UriComponentsBuilder uriBuilder = UriComponentsBuilder
+                                .fromHttpUrl(blackboxEvaluatorApiURL + PARAMETERS_URL);
+                uriBuilder.pathSegment(name);
+                uriBuilder.path(ACTIVE_URL_PATH);
 
-        System.out.println("GG" + uriBuilder.toUriString());
-        EvalParameter parameter = blackboxApi
-                .exchange(uriBuilder.toUriString(), HttpMethod.GET, null, EvalParameter.class)
-                .getBody();
-        return parameter;
-    }
+                System.out.println("GG" + uriBuilder.toUriString());
+                EvalParameter parameter = blackboxApi.exchange(uriBuilder.toUriString(),
+                                HttpMethod.GET, null, EvalParameter.class).getBody();
+                return parameter;
+        }
 
-    public List<EvalParameter> retrieveByName(String name, Date start, Date end) {
+        public List<EvalParameter> retrieveByName(String name, Date start, Date end) {
 
-        UriComponentsBuilder uriBuilder =
-                UriComponentsBuilder.fromHttpUrl(blackboxEvaluatorApiURL + PARAMETERS_URL);
-        uriBuilder.pathSegment(name);
-        uriBuilder.path(FILTERED_URL_PATH);
-        uriBuilder.queryParam(ENABLE_FROM_PARAM, name);
-        uriBuilder.queryParam(ENABLE_UNTIL_PARAM, end);
-        List<EvalParameter> parameters = blackboxApi.exchange(uriBuilder.toUriString(),
-                HttpMethod.GET, null, new ParameterizedTypeReference<List<EvalParameter>>() {
-                }).getBody();
-        return parameters;
-    }
+                UriComponentsBuilder uriBuilder = UriComponentsBuilder
+                                .fromHttpUrl(blackboxEvaluatorApiURL + PARAMETERS_URL);
+                uriBuilder.pathSegment(name);
+                uriBuilder.path(FILTERED_URL_PATH);
+                uriBuilder.queryParam(ENABLE_FROM_PARAM, name);
+                uriBuilder.queryParam(ENABLE_UNTIL_PARAM, end);
+                List<EvalParameter> parameters = blackboxApi.exchange(uriBuilder.toUriString(),
+                                HttpMethod.GET, null,
+                                new ParameterizedTypeReference<List<EvalParameter>>() {
+                                }).getBody();
+                return parameters;
+        }
 
-    public List<EvalParameter> retrieveByName(String name) {
+        public List<EvalParameter> retrieveByName(String name) {
 
-        UriComponentsBuilder uriBuilder =
-                UriComponentsBuilder.fromHttpUrl(blackboxEvaluatorApiURL + PARAMETERS_URL);
-        uriBuilder.pathSegment(name);
+                UriComponentsBuilder uriBuilder = UriComponentsBuilder
+                                .fromHttpUrl(blackboxEvaluatorApiURL + PARAMETERS_URL);
+                uriBuilder.pathSegment(name);
 
-        List<EvalParameter> parameters = blackboxApi.exchange(uriBuilder.toUriString(),
-                HttpMethod.GET, null, new ParameterizedTypeReference<List<EvalParameter>>() {
-                }).getBody();
-        return parameters;
-    }
+                List<EvalParameter> parameters = blackboxApi.exchange(uriBuilder.toUriString(),
+                                HttpMethod.GET, null,
+                                new ParameterizedTypeReference<List<EvalParameter>>() {
+                                }).getBody();
+                return parameters;
+        }
 
-    public List<EvalParameter> retrieveBetweenDates(Date start, Date end) {
+        public List<EvalParameter> retrieveBetweenDates(Date start, Date end) {
 
-        UriComponentsBuilder uriBuilder =
-                UriComponentsBuilder.fromHttpUrl(blackboxEvaluatorApiURL + PARAMETERS_URL);
-        uriBuilder.path(FILTERED_URL_PATH);
-        uriBuilder.queryParam(ENABLE_FROM_PARAM, start);
-        uriBuilder.queryParam(ENABLE_UNTIL_PARAM, end);
-        List<EvalParameter> parameters = blackboxApi.exchange(uriBuilder.toUriString(),
-                HttpMethod.GET, null, new ParameterizedTypeReference<List<EvalParameter>>() {
-                }).getBody();
-        return parameters;
-    }
+                UriComponentsBuilder uriBuilder = UriComponentsBuilder
+                                .fromHttpUrl(blackboxEvaluatorApiURL + PARAMETERS_URL);
+                uriBuilder.path(FILTERED_URL_PATH);
+                uriBuilder.queryParam(ENABLE_FROM_PARAM, start);
+                uriBuilder.queryParam(ENABLE_UNTIL_PARAM, end);
+                List<EvalParameter> parameters = blackboxApi.exchange(uriBuilder.toUriString(),
+                                HttpMethod.GET, null,
+                                new ParameterizedTypeReference<List<EvalParameter>>() {
+                                }).getBody();
+                return parameters;
+        }
 
-    public List<EvalParameter> retrieveAll() {
-        UriComponentsBuilder uriBuilder =
-                UriComponentsBuilder.fromHttpUrl(blackboxEvaluatorApiURL + PARAMETERS_URL);
-        List<EvalParameter> parameters = blackboxApi.exchange(uriBuilder.toUriString(),
-                HttpMethod.GET, null, new ParameterizedTypeReference<List<EvalParameter>>() {
-                }).getBody();
-        return parameters;
-    }
+        public List<EvalParameter> retrieveAll() {
+                UriComponentsBuilder uriBuilder = UriComponentsBuilder
+                                .fromHttpUrl(blackboxEvaluatorApiURL + PARAMETERS_URL);
+                List<EvalParameter> parameters = blackboxApi.exchange(uriBuilder.toUriString(),
+                                HttpMethod.GET, null,
+                                new ParameterizedTypeReference<List<EvalParameter>>() {
+                                }).getBody();
+                return parameters;
+        }
 
-    public List<EvalParameter> retrieveActives() {
-        UriComponentsBuilder uriBuilder =
-                UriComponentsBuilder.fromHttpUrl(blackboxEvaluatorApiURL + PARAMETERS_URL);
-        uriBuilder.queryParam(ACTIVE_PARAM, true);
+        public List<EvalParameter> retrieveActives() {
+                UriComponentsBuilder uriBuilder = UriComponentsBuilder
+                                .fromHttpUrl(blackboxEvaluatorApiURL + PARAMETERS_URL);
+                uriBuilder.queryParam(ACTIVE_PARAM, true);
 
-        List<EvalParameter> parameters = blackboxApi.exchange(uriBuilder.toUriString(),
-                HttpMethod.GET, null, new ParameterizedTypeReference<List<EvalParameter>>() {
-                }).getBody();
-        return parameters;
-    }
+                List<EvalParameter> parameters = blackboxApi.exchange(uriBuilder.toUriString(),
+                                HttpMethod.GET, null,
+                                new ParameterizedTypeReference<List<EvalParameter>>() {
+                                }).getBody();
+                return parameters;
+        }
 
-    public EvalParameter update(EvalParameter parameter) {
-        UriComponentsBuilder uriBuilder =
-                UriComponentsBuilder.fromHttpUrl(blackboxEvaluatorApiURL + PARAMETERS_URL);
-        EvalParameter newParameter = blackboxApi.exchange(uriBuilder.toUriString(),
-                HttpMethod.PUT, null, EvalParameter.class).getBody();
+        public EvalParameter update(EvalParameter parameter) {
+                UriComponentsBuilder uriBuilder = UriComponentsBuilder
+                                .fromHttpUrl(blackboxEvaluatorApiURL + PARAMETERS_URL);
+                uriBuilder.pathSegment(parameter.getName());
+                EvalParameter newParameter = blackboxApi.exchange(uriBuilder.toUriString(),
+                                HttpMethod.PUT, null, EvalParameter.class).getBody();
 
-        return newParameter;
-    }
+                return newParameter;
+        }
 
 }
