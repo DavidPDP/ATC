3c943f1cc257047751ba23bc8246b52a8e3144a7
mgltorsa <mgltorsa@gmail.com>
2020-06-27 04:00:43 -0500

    used uriComponentBuilder instead normal path formed by Strings

--- src/main/java/co/edu/icesi/metrocali/atc/repositories/evaluator/VariableRepository.java
+++ src/main/java/co/edu/icesi/metrocali/atc/repositories/evaluator/VariableRepository.java
@@ -10,13 +10,14 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 import co.edu.icesi.metrocali.atc.entities.evaluator.Variable;
 
 @Repository
 public class VariableRepository extends EvaluatorRepository {
 
     private static final String VARIABLE_URL = "/variables";
-    private static final String FILTERED_BY_URL = "/filteredBy";
+    private static final String FILTERED_URL_PATH = "/filteredBy";
     private static final String IS_KPI_PARAM = "name";
 
     public VariableRepository(@Qualifier("blackboxApi") RestTemplate blackboxApi,
@@ -25,41 +26,50 @@ public class VariableRepository extends EvaluatorRepository {
     }
 
     public Optional<Variable> retrieveByName(String name) {
-        Variable variable =
-                blackboxApi.exchange(blackboxEvaluatorApiURL + VARIABLE_URL + "/" + name,
-                        HttpMethod.GET, null, Variable.class).getBody();
+        UriComponentsBuilder uriBuilder =
+                UriComponentsBuilder.fromHttpUrl(blackboxEvaluatorApiURL + VARIABLE_URL);
+        uriBuilder.pathSegment(name);
+        Variable variable = blackboxApi
+                .exchange(uriBuilder.toUriString(), HttpMethod.GET, null, Variable.class).getBody();
 
         return Optional.ofNullable(variable);
     }
 
     public List<Variable> retrieveAll() {
-        List<Variable> variables = blackboxApi.exchange(blackboxEvaluatorApiURL + VARIABLE_URL,
-                HttpMethod.GET, null, new ParameterizedTypeReference<List<Variable>>() {
+        UriComponentsBuilder uriBuilder =
+                UriComponentsBuilder.fromHttpUrl(blackboxEvaluatorApiURL + VARIABLE_URL);
+        List<Variable> variables = blackboxApi.exchange(uriBuilder.toUriString(), HttpMethod.GET,
+                null, new ParameterizedTypeReference<List<Variable>>() {
                 }).getBody();
         return variables;
     }
 
     public List<Variable> retrieveByIsKPI(boolean isKPI) {
+        UriComponentsBuilder uriBuilder =
+                UriComponentsBuilder.fromHttpUrl(blackboxEvaluatorApiURL + VARIABLE_URL);
+        uriBuilder.path(FILTERED_URL_PATH);
+        uriBuilder.queryParam(IS_KPI_PARAM, isKPI);
         HashMap<String, Object> uriVariables = new HashMap<>();
-        uriVariables.put(IS_KPI_PARAM, isKPI);
-        List<Variable> variables =
-                blackboxApi.exchange(blackboxEvaluatorApiURL + VARIABLE_URL + FILTERED_BY_URL,
-                        HttpMethod.GET, null, new ParameterizedTypeReference<List<Variable>>() {
-                        }, uriVariables).getBody();
+        List<Variable> variables = blackboxApi.exchange(uriBuilder.toUriString(), HttpMethod.GET,
+                null, new ParameterizedTypeReference<List<Variable>>() {
+                }, uriVariables).getBody();
         return variables;
     }
 
     public Optional<Variable> save(Variable variable) {
+        UriComponentsBuilder uriBuilder =
+                UriComponentsBuilder.fromHttpUrl(blackboxEvaluatorApiURL + VARIABLE_URL);
         Variable newVariable = blackboxApi
-                .postForEntity(blackboxEvaluatorApiURL + VARIABLE_URL, variable, Variable.class)
-                .getBody();
+                .postForEntity(uriBuilder.toUriString(), variable, Variable.class).getBody();
         return Optional.ofNullable(newVariable);
     }
 
     public Optional<Variable> update(Variable variable) {
 
-        Variable newVariable = blackboxApi.exchange(blackboxEvaluatorApiURL + VARIABLE_URL,
-                HttpMethod.PUT, new HttpEntity<>(variable), Variable.class).getBody();
+        UriComponentsBuilder uriBuilder =
+                UriComponentsBuilder.fromHttpUrl(blackboxEvaluatorApiURL + VARIABLE_URL);
+        Variable newVariable = blackboxApi.exchange(uriBuilder.toUriString(), HttpMethod.PUT,
+                new HttpEntity<>(variable), Variable.class).getBody();
         return Optional.ofNullable(newVariable);
     }
 
