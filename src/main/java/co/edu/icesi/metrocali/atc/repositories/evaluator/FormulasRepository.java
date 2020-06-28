7cb34e75c96cc46e2e9f17bbd64613e748df90eb
mgltorsa <mgltorsa@gmail.com>
2020-06-27 19:27:19 -0500

    added the way to update variable

--- src/main/java/co/edu/icesi/metrocali/atc/repositories/evaluator/FormulasRepository.java
+++ src/main/java/co/edu/icesi/metrocali/atc/repositories/evaluator/FormulasRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -14,62 +15,74 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class FormulasRepository extends EvaluatorRepository {
 
-    private static final String FORMULAS_URL = "/formulas";
-    private static final String KPI_URL = "/kpi";
-    private static final String ACTIVE_URL = "/active";
+        private static final String FORMULAS_URL = "/formulas";
+        private static final String KPI_URL = "/kpi";
+        private static final String ACTIVE_URL = "/active";
 
 
-    public FormulasRepository(@Qualifier("blackboxApi") RestTemplate blackboxApi,
-            @Value("${blackbox.apis.evaluator}") String blackboxEvaluatorApiURL) {
-        super(blackboxApi, blackboxEvaluatorApiURL);
-    }
+        public FormulasRepository(@Qualifier("blackboxApi") RestTemplate blackboxApi,
+                        @Value("${blackbox.apis.evaluator}") String blackboxEvaluatorApiURL) {
+                super(blackboxApi, blackboxEvaluatorApiURL);
+        }
 
-    public List<Formula> retrieveActives() {
-        List<Formula> formulas =
-                blackboxApi.exchange(blackboxEvaluatorApiURL + FORMULAS_URL + ACTIVE_URL,
-                        HttpMethod.GET, null, new ParameterizedTypeReference<List<Formula>>() {
-                        }).getBody();
-        return formulas;
+        public List<Formula> retrieveActives() {
+                List<Formula> formulas = blackboxApi.exchange(
+                                blackboxEvaluatorApiURL + FORMULAS_URL + ACTIVE_URL, HttpMethod.GET,
+                                null, new ParameterizedTypeReference<List<Formula>>() {
+                                }).getBody();
+                return formulas;
 
-    }
+        }
 
-    public Optional<Formula> retriveByVariable(String name) {
-        UriComponentsBuilder uriBuilder =
-                UriComponentsBuilder.fromHttpUrl(blackboxEvaluatorApiURL + FORMULAS_URL);
-        uriBuilder.pathSegment(name);
-        Formula formula = blackboxApi
-                .exchange(uriBuilder.toUriString(), HttpMethod.GET, null, Formula.class).getBody();
-        return Optional.ofNullable(formula);
+        public Optional<Formula> retriveByVariable(String name) {
+                UriComponentsBuilder uriBuilder = UriComponentsBuilder
+                                .fromHttpUrl(blackboxEvaluatorApiURL + FORMULAS_URL);
+                uriBuilder.pathSegment(name);
+                Formula formula = blackboxApi.exchange(uriBuilder.toUriString(), HttpMethod.GET,
+                                null, Formula.class).getBody();
+                return Optional.ofNullable(formula);
 
-    }
+        }
 
-    public List<Formula> retrieveActivesByKPI() {
-        UriComponentsBuilder uriBuilder =
-                UriComponentsBuilder.fromHttpUrl(blackboxEvaluatorApiURL + FORMULAS_URL);
-        uriBuilder.path(KPI_URL);
-        uriBuilder.path(ACTIVE_URL);
-        List<Formula> formulas = blackboxApi.exchange(uriBuilder.toUriString(), HttpMethod.GET,
-                null, new ParameterizedTypeReference<List<Formula>>() {
-                }).getBody();
-        return formulas;
-    }
+        public List<Formula> retrieveActivesByKPI() {
+                UriComponentsBuilder uriBuilder = UriComponentsBuilder
+                                .fromHttpUrl(blackboxEvaluatorApiURL + FORMULAS_URL);
+                uriBuilder.path(KPI_URL);
+                uriBuilder.path(ACTIVE_URL);
+                List<Formula> formulas =
+                                blackboxApi.exchange(uriBuilder.toUriString(), HttpMethod.GET, null,
+                                                new ParameterizedTypeReference<List<Formula>>() {
+                                                }).getBody();
+                return formulas;
+        }
 
-    public Optional<Formula> save(Formula formula) {
-        UriComponentsBuilder uriBuilder =
-                UriComponentsBuilder.fromHttpUrl(blackboxEvaluatorApiURL + FORMULAS_URL);
-        Formula newFormula = blackboxApi
-                .postForEntity(uriBuilder.toUriString(), formula, Formula.class).getBody();
-        return Optional.ofNullable(newFormula);
-    }
+        public Optional<Formula> save(Formula formula) {
+                UriComponentsBuilder uriBuilder = UriComponentsBuilder
+                                .fromHttpUrl(blackboxEvaluatorApiURL + FORMULAS_URL);
+                Formula newFormula = blackboxApi
+                                .postForEntity(uriBuilder.toUriString(), formula, Formula.class)
+                                .getBody();
+                return Optional.ofNullable(newFormula);
+        }
 
-    public Optional<Formula> retrieveActivesByVariable(String name) {
-        UriComponentsBuilder uriBuilder =
-                UriComponentsBuilder.fromHttpUrl(blackboxEvaluatorApiURL + FORMULAS_URL);
-        uriBuilder.pathSegment(name);
-        uriBuilder.path(ACTIVE_URL);
-        Formula formula = blackboxApi
-                .exchange(uriBuilder.toUriString(), HttpMethod.GET, null, Formula.class).getBody();
-        return Optional.ofNullable(formula);
-    }
+        public Optional<Formula> retrieveActivesByVariable(String name) {
+                UriComponentsBuilder uriBuilder = UriComponentsBuilder
+                                .fromHttpUrl(blackboxEvaluatorApiURL + FORMULAS_URL);
+                uriBuilder.pathSegment(name);
+                uriBuilder.path(ACTIVE_URL);
+                Formula formula = blackboxApi.exchange(uriBuilder.toUriString(), HttpMethod.GET,
+                                null, Formula.class).getBody();
+                return Optional.ofNullable(formula);
+        }
+
+        public Optional<Formula> update(String variableName, Formula formula) {
+                UriComponentsBuilder uriBuilder = UriComponentsBuilder
+                                .fromHttpUrl(blackboxEvaluatorApiURL + FORMULAS_URL);
+                Formula newFormula = blackboxApi
+                                .exchange(uriBuilder.toUriString(), HttpMethod.PUT,
+                                                new HttpEntity<Formula>(formula), Formula.class)
+                                .getBody();
+                return Optional.ofNullable(newFormula);
+        }
 
 }
--- src/main/java/co/edu/icesi/metrocali/atc/services/evaluator/VariableService.java
+++ src/main/java/co/edu/icesi/metrocali/atc/services/evaluator/VariableService.java
@@ -1,9 +1,12 @@
 package co.edu.icesi.metrocali.atc.services.evaluator;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import co.edu.icesi.metrocali.atc.entities.evaluator.Formula;
 import co.edu.icesi.metrocali.atc.entities.evaluator.Variable;
@@ -56,27 +59,66 @@ public class VariableService {
         return variables;
     }
 
+    public Optional<Variable> updateVariable(String name, String classification, String description,
+            String expression, boolean isKPI) throws Exception {
+        Date now = Date.from(new Timestamp(System.currentTimeMillis()).toInstant());
+        Variable newVariable = new Variable(name, classification, description, isKPI, null);
+        Optional<Variable> variableWrapper = variableRepository.retrieveByName(name);
+        if (variableWrapper.isPresent()) {
+            newVariable = variableWrapper.get();
+            newVariable.setClassification(classification);
+            newVariable.setDescriptionVar(description);
+            newVariable.setIsKPI(isKPI);
+
+            Formula newFormula = new Formula();
+            newFormula.setStartDate(now);
+            newFormula.setEndDate(null);
+            newFormula.setExpression(expression);
+
+            Optional<Formula> lastFormulaWrapper =
+                    formulasRepository.retrieveActivesByVariable(name);
+
+            if (!lastFormulaWrapper.isPresent()) {
+                throw new Exception("Variable: " + name + " no tiene una última fórmula asignada");
+            }
+
+            Formula lastFormula = lastFormulaWrapper.get();
+
+            lastFormula.setEndDate(now);
+            formulasRepository.update(name, lastFormula);
+
+            Optional<Formula> newFormulaWrapper = formulasRepository.save(newFormula);
+            if (!newFormulaWrapper.isPresent()) {
+                throw new Exception("Formula para: " + name + " no fue asignada");
+            }
+            newFormula = newFormulaWrapper.get();
+
+            variableWrapper = variableRepository.update(newVariable);
+            if (!variableWrapper.isPresent()) {
+                throw new Exception("No se pudo actualizar la variable: " + name);
+            }
+            newVariable = variableWrapper.get();
+            newVariable.setLastFormulaExpression(lastFormula.getExpression());
+
+            return Optional.of(newVariable);
+
+        } else {
+            throw new Exception("Variable: " + name + " no existe");
+        }
+    }
+
     public Optional<Variable> saveVariable(String name, String classification, String description,
             String expression, boolean isKPI) {
         Optional<Variable> newVariable = Optional.empty();
-        Optional<Variable> variableWrapper = variableRepository.retrieveByName(name);
-        Variable variable = new Variable(name, classification, description, isKPI, null);
-        if (variableWrapper.isPresent()) {
-            variable = variableWrapper.get();
-            variable.setClassification(classification);
-            variable.setDescriptionVar(description);
-            variable.setIsKPI(isKPI);
-            newVariable = variableRepository.update(variable);
-        } else {
-            newVariable = variableRepository.save(variable);
-        }
+        Variable variable = new Variable(name, classification, description, isKPI, expression);
+
+        newVariable = variableRepository.save(variable);
 
         Formula formula = new Formula();
         formula.setExpression(expression);
 
         formulasRepository.save(formula);
         return newVariable;
-
     }
 
 }
