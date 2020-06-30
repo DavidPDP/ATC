7ad3f3a11664727586904b38b17488df00690155
mgltorsa <mgltorsa@gmail.com>
2020-06-29 22:00:51 -0500

    removed unused imports

--- src/main/java/co/edu/icesi/metrocali/atc/api/evaluator/HTTPMeasurementsAPI.java
+++ src/main/java/co/edu/icesi/metrocali/atc/api/evaluator/HTTPMeasurementsAPI.java
@@ -3,7 +3,6 @@ package co.edu.icesi.metrocali.atc.api.evaluator;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
